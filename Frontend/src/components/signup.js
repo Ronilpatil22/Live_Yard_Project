dfb3d601430bb5dccb8de8e3bd389d2711640f0f
Ronil Patil <98299193+Ronilpatil22@users.noreply.github.com>
2024-03-12 21:08:55 +0530

    Login and signup frontend

---
+++ Frontend/src/components/signup.js
@@ -0,0 +1,85 @@
+import { useEffect } from "react";
+import "./signup.css";
+export default function Signup() {
+  const themes = [
+    {
+      background: "#1A1A2E",
+      color: "#FFFFFF",
+      primaryColor: "#0F3460",
+    },
+    {
+      background: "#461220",
+      color: "#FFFFFF",
+      primaryColor: "#E94560",
+    },
+    {
+      background: "#192A51",
+      color: "#FFFFFF",
+      primaryColor: "#967AA1",
+    },
+    {
+      background: "#F7B267",
+      color: "#000000",
+      primaryColor: "#F4845F",
+    },
+    {
+      background: "#F25F5C",
+      color: "#000000",
+      primaryColor: "#642B36",
+    },
+    {
+      background: "#231F20",
+      color: "#FFF",
+      primaryColor: "#BB4430",
+    },
+  ];
+  useEffect(() => {
+    const setTheme = (theme) => {
+      const root = document.querySelector(":root");
+      root.style.setProperty("--background", theme.background);
+      root.style.setProperty("--color", theme.color);
+      root.style.setProperty("--primary-color", theme.primaryColor);
+      root.style.setProperty("--glass-color", theme.glassColor);
+    };
+
+    const displayThemeButtons = () => {
+      const btnContainer = document.querySelector(".theme-btn-container");
+      themes.forEach((theme) => {
+        const div = document.createElement("div");
+        div.className = "theme-btn";
+        div.style.cssText = `background: ${theme.background}; width: 25px; height: 25px`;
+        btnContainer.appendChild(div);
+        div.addEventListener("click", () => setTheme(theme));
+      });
+    };
+
+    displayThemeButtons();
+  });
+
+  return (
+    <section className="container">
+      <div className="login-container">
+        <div className="circle circle-one" />
+        <div className="form-container">
+          <img
+            src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png"
+            alt="illustration"
+            className="illustration"
+          />
+          <h1 className="opacity">SIGNUP</h1>
+          <form>
+            <input type="text" placeholder="FULLNAME" />
+            <input type="text" placeholder="USERNAME" />
+            <input type="password" placeholder="PASSWORD" />
+            <input type="password" placeholder="CONFIRM PASSWORD" />
+            <input type="email" placeholder="EMAIL" />
+            <input type="text" placeholder="GENDER" />
+            <button className="opacity">SUBMIT</button>
+          </form>
+        </div>
+        <div className="circle circle-two" />
+      </div>
+      <div className="theme-btn-container" />
+    </section>
+  );
+}
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <Signup />
+  }
+])
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
-
