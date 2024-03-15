fbec1a4b492dc4388fe3891ab94ca52c79ebbd69
Ronil Patil <98299193+Ronilpatil22@users.noreply.github.com>
2024-03-15 12:38:29 +0530

    login&submit

--- Frontend/src/components/login.js
+++ Frontend/src/components/login.js
@@ -1,52 +1,53 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import "./login.css";
 import { Link } from "react-router-dom";
 import useLogin from "../hooks/useLogin";
 import { useAuthContext } from "../Context/AuthContext";
 export default function Login() {
-  const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const {login} = useLogin();
-    const handleSubmit = async(e)=>{
-      e.preventDefault();
-      await login(username,password);
-  }
+  const { login } = useLogin();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
 
-  const {authUser}=useAuthContext();
-  const themes = [
-    {
-      background: "#1A1A2E",
-      color: "#FFFFFF",
-      primaryColor: "#0F3460",
-    },
-    {
-      background: "#461220",
-      color: "#FFFFFF",
-      primaryColor: "#E94560",
-    },
-    {
-      background: "#192A51",
-      color: "#FFFFFF",
-      primaryColor: "#967AA1",
-    },
-    {
-      background: "#F7B267",
-      color: "#000000",
-      primaryColor: "#F4845F",
-    },
-    {
-      background: "#F25F5C",
-      color: "#000000",
-      primaryColor: "#642B36",
-    },
-    {
-      background: "#231F20",
-      color: "#FFF",
-      primaryColor: "#BB4430",
-    },
-  ];
+  const { authUser } = useAuthContext();
+
   useEffect(() => {
+    const themes = [
+      {
+        background: "#1A1A2E",
+        color: "#FFFFFF",
+        primaryColor: "#0F3460",
+      },
+      {
+        background: "#461220",
+        color: "#FFFFFF",
+        primaryColor: "#E94560",
+      },
+      {
+        background: "#192A51",
+        color: "#FFFFFF",
+        primaryColor: "#967AA1",
+      },
+      {
+        background: "#F7B267",
+        color: "#000000",
+        primaryColor: "#F4845F",
+      },
+      {
+        background: "#F25F5C",
+        color: "#000000",
+        primaryColor: "#642B36",
+      },
+      {
+        background: "#231F20",
+        color: "#FFF",
+        primaryColor: "#BB4430",
+      },
+    ];
     const setTheme = (theme) => {
       const root = document.querySelector(":root");
       root.style.setProperty("--background", theme.background);
@@ -67,10 +68,7 @@ export default function Login() {
     };
 
     displayThemeButtons();
-  });
-  async function handleSubmit(e){
-    
-  }
+  }, []);
 
   return (
     <section className="container">
@@ -84,18 +82,34 @@ export default function Login() {
           />
           <h1 className="opacity">LOGIN</h1>
           <form onSubmit={handleSubmit}>
-            <input type="text" value={username}  onChange={(e)=>setUsername(e.target.value)}placeholder="USERNAME" />
-            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="PASSWORD" />
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => {
+                e.preventDefault();
+                setUsername(e.target.value);
+              }}
+              placeholder="USERNAME"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                e.preventDefault();
+                setPassword(e.target.value);
+              }}
+              placeholder="PASSWORD"
+            />
             <button className="opacity">SUBMIT</button>
           </form>
           <div className="register-forget opacity">
-          <Link to="/signup">REGISTER</Link>
+            <Link to="/signup">REGISTER</Link>
             <Link to="/forgot">FORGOT PASSWORD</Link>
           </div>
         </div>
         <div className="circle circle-two" />
       </div>
-      <div className="theme-btn-container" />
+      <div key="1" className="theme-btn-container" />
     </section>
   );
 }
--- Frontend/src/components/signup.js
+++ Frontend/src/components/signup.js
@@ -2,54 +2,55 @@ import { useEffect, useState } from "react";
 import "./signup.css";
 import useSignup from "../hooks/useSignUp";
 export default function Signup() {
-  const {signup} = useSignup();
-  const [inputs,setInputs] = useState({
-    fullName:'',
-    username:'',
-    password:'',
-    confirmPassword:'',
-    email:'',
-    gender:''
-  })
+  const { signup } = useSignup();
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+    gender: "",
+  });
 
-  const  handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(inputs);
     await signup(inputs);
-  }
-  const themes = [
-    {
-      background: "#1A1A2E",
-      color: "#FFFFFF",
-      primaryColor: "#0F3460",
-    },
-    {
-      background: "#461220",
-      color: "#FFFFFF",
-      primaryColor: "#E94560",
-    },
-    {
-      background: "#192A51",
-      color: "#FFFFFF",
-      primaryColor: "#967AA1",
-    },
-    {
-      background: "#F7B267",
-      color: "#000000",
-      primaryColor: "#F4845F",
-    },
-    {
-      background: "#F25F5C",
-      color: "#000000",
-      primaryColor: "#642B36",
-    },
-    {
-      background: "#231F20",
-      color: "#FFF",
-      primaryColor: "#BB4430",
-    },
-  ];
+  };
+
   useEffect(() => {
+    const themes = [
+      {
+        background: "#1A1A2E",
+        color: "#FFFFFF",
+        primaryColor: "#0F3460",
+      },
+      {
+        background: "#461220",
+        color: "#FFFFFF",
+        primaryColor: "#E94560",
+      },
+      {
+        background: "#192A51",
+        color: "#FFFFFF",
+        primaryColor: "#967AA1",
+      },
+      {
+        background: "#F7B267",
+        color: "#000000",
+        primaryColor: "#F4845F",
+      },
+      {
+        background: "#F25F5C",
+        color: "#000000",
+        primaryColor: "#642B36",
+      },
+      {
+        background: "#231F20",
+        color: "#FFF",
+        primaryColor: "#BB4430",
+      },
+    ];
     const setTheme = (theme) => {
       const root = document.querySelector(":root");
       root.style.setProperty("--background", theme.background);
@@ -70,7 +71,7 @@ export default function Signup() {
     };
 
     displayThemeButtons();
-  });
+  }, []);
 
   return (
     <section className="container">
@@ -84,12 +85,50 @@ export default function Signup() {
           />
           <h1 className="opacity">SIGNUP</h1>
           <form onSubmit={handleSubmit}>
-            <input type="text" value={inputs.fullName} onChange={(e)=>setInputs({...inputs,fullName:e.target.value})} placeholder="FULLNAME" />
-            <input type="text" value={inputs.username} onChange={(e)=>setInputs({...inputs,username:e.target.value})} placeholder="USERNAME" />
-            <input type="text" value={inputs.password} onChange={(e)=>setInputs({...inputs,password:e.target.value})} placeholder="PASSWORD" />
-            <input type="text" value={inputs.confirmPassword} onChange={(e)=>setInputs({...inputs,confirmPassword:e.target.value})} placeholder="CONFIRM PASSWORD" />
-            <input type="email" value={inputs.email} onChange={(e)=>setInputs({...inputs,email:e.target.value})} placeholder="EMAIL" />
-            <input type="text" value={inputs.gender} onChange={(e)=>setInputs({...inputs,gender:e.target.value})} placeholder="GENDER" />
+            <input
+              type="text"
+              value={inputs.fullName}
+              onChange={(e) =>
+                setInputs({ ...inputs, fullName: e.target.value })
+              }
+              placeholder="FULLNAME"
+            />
+            <input
+              type="text"
+              value={inputs.username}
+              onChange={(e) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
+              placeholder="USERNAME"
+            />
+            <input
+              type="text"
+              value={inputs.password}
+              onChange={(e) =>
+                setInputs({ ...inputs, password: e.target.value })
+              }
+              placeholder="PASSWORD"
+            />
+            <input
+              type="text"
+              value={inputs.confirmPassword}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
+              placeholder="CONFIRM PASSWORD"
+            />
+            <input
+              type="email"
+              value={inputs.email}
+              onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+              placeholder="EMAIL"
+            />
+            <input
+              type="text"
+              value={inputs.gender}
+              onChange={(e) => setInputs({ ...inputs, gender: e.target.value })}
+              placeholder="GENDER"
+            />
             <button className="opacity">SUBMIT</button>
           </form>
         </div>
