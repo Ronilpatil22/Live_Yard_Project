c77c1f328fbd2c619537112e1e9db7fc09409461
Ronil Patil <98299193+Ronilpatil22@users.noreply.github.com>
2024-04-06 16:01:07 +0530

    UI update

--- Frontend/src/components/login.js
+++ Frontend/src/components/login.js
@@ -15,101 +15,59 @@ export default function Login() {
 
   const { authUser } = useAuthContext();
 
-  useEffect(() => {
-    const themes = [
-      {
-        background: "#1A1A2E",
-        color: "#FFFFFF",
-        primaryColor: "#0F3460",
-      },
-      {
-        background: "#461220",
-        color: "#FFFFFF",
-        primaryColor: "#E94560",
-      },
-      {
-        background: "#192A51",
-        color: "#FFFFFF",
-        primaryColor: "#967AA1",
-      },
-      {
-        background: "#F7B267",
-        color: "#000000",
-        primaryColor: "#F4845F",
-      },
-      {
-        background: "#F25F5C",
-        color: "#000000",
-        primaryColor: "#642B36",
-      },
-      {
-        background: "#231F20",
-        color: "#FFF",
-        primaryColor: "#BB4430",
-      },
-    ];
-    const setTheme = (theme) => {
-      const root = document.querySelector(":root");
-      root.style.setProperty("--background", theme.background);
-      root.style.setProperty("--color", theme.color);
-      root.style.setProperty("--primary-color", theme.primaryColor);
-      root.style.setProperty("--glass-color", theme.glassColor);
-    };
-
-    const displayThemeButtons = () => {
-      const btnContainer = document.querySelector(".theme-btn-container");
-      themes.forEach((theme) => {
-        const div = document.createElement("div");
-        div.className = "theme-btn";
-        div.style.cssText = `background: ${theme.background}; width: 25px; height: 25px`;
-        btnContainer.appendChild(div);
-        div.addEventListener("click", () => setTheme(theme));
-      });
-    };
-
-    displayThemeButtons();
-  }, []);
-
   return (
-    <section className="container">
-      <div className="login-container">
-        <div className="circle circle-one" />
-        <div className="form-container">
-          <img
-            src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png"
-            alt="illustration"
-            className="illustration"
-          />
-          <h1 className="opacity">LOGIN</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => {
-                e.preventDefault();
-                setUsername(e.target.value);
-              }}
-              placeholder="USERNAME"
-            />
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => {
-                e.preventDefault();
-                setPassword(e.target.value);
-              }}
-              placeholder="PASSWORD"
-            />
-            <button className="opacity">SUBMIT</button>
-          </form>
-          <div className="register-forget opacity">
-            <Link to="/signup">REGISTER</Link>
-            <Link to="/forgot">FORGOT PASSWORD</Link>
-          </div>
-        </div>
-        <div className="circle circle-two" />
+    <form className="form-signin" onSubmit={handleSubmit}>
+      <img
+        className="mb-4"
+        src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg"
+        alt=""
+        width={72}
+        height={72}
+      />
+      <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label htmlFor="inputEmail" className="sr-only">
+        Email address
+      </label>
+      <input
+        type="email"
+        id="inputEmail"
+        className="form-control"
+        placeholder="USERNAME"
+        required=""
+        autoFocus=""
+        value={username}
+        onChange={(e) => {
+          e.preventDefault();
+          setUsername(e.target.value);
+        }}
+      />
+      <label htmlFor="inputPassword" className="sr-only">
+        Password
+      </label>
+      <input
+        type="password"
+        id="inputPassword"
+        className="form-control"
+        required=""
+        value={password}
+        onChange={(e) => {
+          e.preventDefault();
+          setPassword(e.target.value);
+        }}
+        placeholder="PASSWORD"
+      />
+      <div className="checkbox mb-3">
+        <label>
+          <input type="checkbox" defaultValue="remember-me" /> Remember me
+        </label>
       </div>
-      <div key="1" className="theme-btn-container" />
-    </section>
+      <button className="btn btn-lg btn-primary btn-block" type="submit">
+        Sign in
+      </button>
+      <p className="">
+        New to LiveYard?<Link to="/signup">Create an account</Link>
+      </p>
+      <p className="mt-5 mb-3 text-muted">© 2017-2018</p>
+    </form>
   );
 }
--- Frontend/src/components/signup.js
+++ Frontend/src/components/signup.js
@@ -18,123 +18,97 @@ export default function Signup() {
     await signup(inputs);
   };
 
-  useEffect(() => {
-    const themes = [
-      {
-        background: "#1A1A2E",
-        color: "#FFFFFF",
-        primaryColor: "#0F3460",
-      },
-      {
-        background: "#461220",
-        color: "#FFFFFF",
-        primaryColor: "#E94560",
-      },
-      {
-        background: "#192A51",
-        color: "#FFFFFF",
-        primaryColor: "#967AA1",
-      },
-      {
-        background: "#F7B267",
-        color: "#000000",
-        primaryColor: "#F4845F",
-      },
-      {
-        background: "#F25F5C",
-        color: "#000000",
-        primaryColor: "#642B36",
-      },
-      {
-        background: "#231F20",
-        color: "#FFF",
-        primaryColor: "#BB4430",
-      },
-    ];
-    const setTheme = (theme) => {
-      const root = document.querySelector(":root");
-      root.style.setProperty("--background", theme.background);
-      root.style.setProperty("--color", theme.color);
-      root.style.setProperty("--primary-color", theme.primaryColor);
-      root.style.setProperty("--glass-color", theme.glassColor);
-    };
-
-    const displayThemeButtons = () => {
-      const btnContainer = document.querySelector(".theme-btn-container");
-      themes.forEach((theme) => {
-        const div = document.createElement("div");
-        div.className = "theme-btn";
-        div.style.cssText = `background: ${theme.background}; width: 25px; height: 25px`;
-        btnContainer.appendChild(div);
-        div.addEventListener("click", () => setTheme(theme));
-      });
-    };
-
-    displayThemeButtons();
-  }, []);
-
   return (
-    <section className="container">
-      <div className="login-container">
-        <div className="circle circle-one" />
-        <div className="form-container">
-          <img
-            src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png"
-            alt="illustration"
-            className="illustration"
-          />
-          <h1 className="opacity">SIGNUP</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={inputs.fullName}
-              onChange={(e) =>
-                setInputs({ ...inputs, fullName: e.target.value })
-              }
-              placeholder="FULLNAME"
-            />
-            <input
-              type="text"
-              value={inputs.username}
-              onChange={(e) =>
-                setInputs({ ...inputs, username: e.target.value })
-              }
-              placeholder="USERNAME"
-            />
-            <input
-              type="text"
-              value={inputs.password}
-              onChange={(e) =>
-                setInputs({ ...inputs, password: e.target.value })
-              }
-              placeholder="PASSWORD"
-            />
-            <input
-              type="text"
-              value={inputs.confirmPassword}
-              onChange={(e) =>
-                setInputs({ ...inputs, confirmPassword: e.target.value })
-              }
-              placeholder="CONFIRM PASSWORD"
-            />
-            <input
-              type="email"
-              value={inputs.email}
-              onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-              placeholder="EMAIL"
-            />
-            <input
-              type="text"
-              value={inputs.gender}
-              onChange={(e) => setInputs({ ...inputs, gender: e.target.value })}
-              placeholder="GENDER"
-            />
-            <button className="opacity">SUBMIT</button>
-          </form>
-        </div>
-        <div className="circle circle-two" />
-      </div>
-      <div className="theme-btn-container" />
-    </section>
+    <form className="form-signin" onSubmit={handleSubmit}>
+      <img
+        className="mb-4"
+        src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg"
+        alt=""
+        width={72}
+        height={72}
+      />
+      <h1 className="h3 mb-3 font-weight-normal">Please sign Up</h1>
+      <label htmlFor="inputFullName" className="sr-only">
+        Full Name
+      </label>
+      <input
+        type="text"
+        id="fullName"
+        className="form-control"
+        value={inputs.fullName}
+        onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+        placeholder="FULLNAME"
+        required=""
+        autoFocus=""
+      />
+      <label htmlFor="inputUsername" className="sr-only">
+        Username
+      </label>
+      <input
+        type="text"
+        id="inputUsername"
+        className="form-control"
+        value={inputs.username}
+        onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+        placeholder="USERNAME"
+        required=""
+      />
+      <label htmlFor="inputPassword" className="sr-only">
+        Password
+      </label>
+      <input
+        type="password"
+        id="inputPassword"
+        className="form-control"
+        value={inputs.password}
+        onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+        placeholder="PASSWORD"
+        required=""
+      />
+      <label htmlFor="inputConfirmPassword" className="sr-only">
+        Confirm Password
+      </label>
+      <input
+        type="password"
+        id="inputConfirmPassword"
+        className="form-control"
+        value={inputs.confirmPassword}
+        onChange={(e) =>
+          setInputs({ ...inputs, confirmPassword: e.target.value })
+        }
+        placeholder="CONFIRM PASSWORD"
+        required=""
+      />
+
+      <label htmlFor="inputEmail" className="sr-only">
+        Email
+      </label>
+      <input
+        type="email"
+        id="inputEmail"
+        className="form-control"
+        value={inputs.email}
+        onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+        placeholder="EMAIL"
+        required=""
+      />
+
+      <label htmlFor="inputGender" className="sr-only">
+        Gender
+      </label>
+      <input
+        type="text"
+        id="inputGender"
+        className="form-control"
+        value={inputs.gender}
+        onChange={(e) => setInputs({ ...inputs, gender: e.target.value })}
+        placeholder="GENDER"
+        required=""
+      />
+      <button className="btn btn-lg btn-primary btn-block" type="submit">
+        Sign Up
+      </button>
+      <p className="mt-5 mb-3 text-muted">© 2017-2018</p>
+    </form>
   );
 }
